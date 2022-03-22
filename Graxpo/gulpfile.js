2e905169b827a97c7b3e207fb5cf3bcd00f5019c
StevanZivadinovic <stevanzivadinovic@outlook.com>
2022-03-22 22:14:06 +0100

    header

--- Graxpo/gulpfile.js
+++ Graxpo/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
-const bowserSync = require('browser-sync').create()
+// const bowserSync = require('browser-sync').create()//ovo nisam instalirao, mora se instalira da bi radio zadnji deo koda, sad 
+//kombinujem gulp v3 i v4 verzije
 
 
 // gulp.task('sass', function(){ //v3, prvi deo koda
@@ -13,21 +14,21 @@ function style(){ //v4, prvi deo koda
   return gulp.src('./style/**/*.scss')
   .pipe(sass()) // Converts Sass to CSS with gulp-sass
   .pipe(gulp.dest('./css'))
-  .pipe(bowserSync.stream)
+  // .pipe(bowserSync.stream)
 }
 exports.style = style;
 
-// gulp.task('watch', function(){ //v3 gulp, pokreces sa gulp watch, drugi deo
-//   gulp.watch('./style/**/*.scss', gulp.series('style')); //ovo style u gulp.series() je funkcija style() definisana iznad
-//   // Other watchers
-// })
+gulp.task('watch', function(){ //v3 gulp, pokreces sa gulp watch, drugi deo
+  gulp.watch('./style/**/*.scss', gulp.series('style')); //ovo style u gulp.series() je funkcija style() definisana iznad
+  // Other watchers
+})
 
-function watch(){  //v4 gulp, pokreces sa gulp watch, drugi deo
-  bowserSync.init({
-    server:{
-      baseDir:'./'
-    }
-  });
+// function watch(){  //v4 gulp, pokreces sa gulp watch, drugi deo
+//   bowserSync.init({
+//     server:{
+//       baseDir:'./'
+//     }
+//   });
 
-  gulp.watch('./style/**/*.scss', style)
-}
+//   gulp.watch('./style/**/*.scss', style)
+// }
