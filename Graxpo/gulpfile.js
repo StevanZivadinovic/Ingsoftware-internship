46207539b44c3edaf45381e165843dbe2eb143c2
StevanZivadinovic <stevanzivadinovic@outlook.com>
2022-03-22 19:41:40 +0100

    provaljenGulp

--- Graxpo/gulpfile.js
+++ Graxpo/gulpfile.js
@@ -1,9 +1,33 @@
 var gulp = require('gulp');
-// Requires the gulp-sass plugin
 var sass = require('gulp-sass')(require('sass'));
+const bowserSync = require('browser-sync').create()
 
-gulp.task('sass', function(){
-  return gulp.src('GRAXPO/style/**/*.scss')
-    .pipe(sass()) // Converts Sass to CSS with gulp-sass
-    .pipe(gulp.dest('GRAXPO/css'))
-});
+
+// gulp.task('sass', function(){
+//   return gulp.src('./style/**/*.scss')
+//     .pipe(sass()) // Converts Sass to CSS with gulp-sass
+//     .pipe(gulp.dest('./css'))
+// });
+
+function style(){
+  return gulp.src('./style/**/*.scss')
+  .pipe(sass()) // Converts Sass to CSS with gulp-sass
+  .pipe(gulp.dest('./css'))
+  .pipe(bowserSync.stream)
+}
+exports.style = style;
+
+// gulp.task('watch', function(){ //v3 gulp, pokreces sa gulp watch
+//   gulp.watch('./style/**/*.scss', gulp.series('style')); //ovo style u gulp.series() je funkcija style() definisana iznad
+//   // Other watchers
+// })
+
+function watch(){  //v4 gulp, pokreces sa gulp watch
+  bowserSync.init({
+    server:{
+      baseDir:'./'
+    }
+  });
+
+  gulp.watch('./style/**/*.scss', style)
+}
