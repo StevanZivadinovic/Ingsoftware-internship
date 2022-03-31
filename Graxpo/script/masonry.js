23dba21b721ee87c46ac376a0e57ee9ab232f51e
StevanZivadinovic <stevanzivadinovic@outlook.com>
2022-03-31 11:51:30 +0200

    update masonry plugin

--- Graxpo/script/masonry.js
+++ Graxpo/script/masonry.js
@@ -1,12 +1,16 @@
-// import Masonry from 'masonry-layout';
-// var elem = document.querySelector('.gridA');
-//   var msnry = new Masonry( elem, {
-//     // options
-//     itemSelector: '.grid-item',
-//     columnWidth: 100
-//   });
+import Masonry from 'masonry-layout';
+
+window.onload=()=>{
+
+    const grid = document.querySelector('.gridA');
+      var msnry = new Masonry( grid, {
+        // options
+        itemSelector: '.grid-item',
+        // columnWidth: 100
+      });
+}
 
 
-document.querySelector('.allCategories').addEventListener('click',e=>{
-    document.querySelector('.gridA').style.display='grid';
-})
+// document.querySelector('.allCategories').addEventListener('click',e=>{
+//     document.querySelector('.gridA').style.display='grid';
+// })
