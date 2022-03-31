70cf1da0c22be560eff6ddafb5edea37df9a4eef
StevanZivadinovic <stevanzivadinovic@outlook.com>
2022-03-31 14:24:45 +0200

    change

--- Graxpo/script/masonry.js
+++ Graxpo/script/masonry.js
@@ -1,16 +1,42 @@
-import Masonry from 'masonry-layout';
+// import Masonry from 'masonry-layout';
+window.onload = () => {
+  const grid = document.querySelector(".gridA");
+  var msnry = new Masonry(grid, {
+    // options
+    itemSelector: ".grid-item",
+    // columnWidth: 100
+  });
+};
+document.querySelector(".allCategories").addEventListener("click", (e) => {
+  document.querySelector(".gridA").style.display = "grid";
+  Array.from(document.querySelectorAll(".grid-item")).forEach((a) => {
+    a.style.display = "grid";
+    const grid = document.querySelector(".gridA");
+    var msnry = new Masonry(grid, {
+      // options
+      itemSelector: ".grid-item",
+      gutter:2
+    });
+  });
+});
 
-window.onload=()=>{
-
-    const grid = document.querySelector('.gridA');
-      var msnry = new Masonry( grid, {
-        // options
-        itemSelector: '.grid-item',
-        // columnWidth: 100
-      });
+let showFuntion = (elements, elementsMasonry)=>{
+    // document.querySelector(button).addEventListener("click", (e) => {
+        Array.from(document.querySelectorAll(".grid-item")).forEach((a) => {
+          if (a.className.includes(elements)) {
+            a.style.display = "grid";
+      
+            const grid = document.querySelector(".gridA");
+            var msnry = new Masonry(grid, {
+              // options
+              itemSelector: elementsMasonry,
+              gutter:2
+              // columnWidth: 100
+            });
+          } else {
+            a.style.display = "none";
+          }
+        });
+    //   });
 }
 
-
-// document.querySelector('.allCategories').addEventListener('click',e=>{
-//     document.querySelector('.gridA').style.display='grid';
-// })
