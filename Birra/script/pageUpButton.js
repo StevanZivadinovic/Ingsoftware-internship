e77bbebdd6b0de571837d4ccbf306495a7bc44ef
StevanZivadinovic <stevanzivadinovic@outlook.com>
2022-04-10 20:39:25 +0200

    add buttonUp

--- Birra/script/pageUpButton.js
+++ Birra/script/pageUpButton.js
@@ -13,12 +13,12 @@ else{
 
 window.addEventListener('scroll',e=>{
     if(window.scrollY==0){
-        console.log('vrh!');
+        
         document.querySelector('.pageUpButton').style.bottom = '-3.5rem';
         document.querySelector('.pageUpButton').style.opacity = '0'
     }
     else{
-        console.log('nije na vrhu!');
+       
         document.querySelector('.pageUpButton').style.bottom = '2.5rem';
         document.querySelector('.pageUpButton').style.opacity = '1';
     }
