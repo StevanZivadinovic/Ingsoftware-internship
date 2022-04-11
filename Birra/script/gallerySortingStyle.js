d186f3dad9a5ea06850d663d2f5b45f40cf7fbf2
StevanZivadinovic <stevanzivadinovic@outlook.com>
2022-04-11 17:08:25 +0200

    set display style of sorted gallery items

---
+++ Birra/script/gallerySortingStyle.js
@@ -0,0 +1,11 @@
+
+document.querySelector('.list').addEventListener('click',e=>{
+    console.log('haj')
+    document.querySelector('.galleryGallery').classList.add('listGallery')
+});
+
+
+document.querySelector('.grid').addEventListener('click',e=>{
+    console.log('haj')
+    document.querySelector('.galleryGallery').classList.remove('listGallery')
+})
