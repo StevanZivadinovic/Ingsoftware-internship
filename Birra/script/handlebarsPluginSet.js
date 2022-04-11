9ca4289c0ad0b577b49e3c4e59dfe32f834cff11
StevanZivadinovic <stevanzivadinovic@outlook.com>
2022-04-11 16:35:00 +0200

    set filter option

--- Birra/script/handlebarsPluginSet.js
+++ Birra/script/handlebarsPluginSet.js
@@ -1,54 +1,109 @@
 let beers = [];
 var source = document.getElementById("entry-template").innerHTML;
 var template = Handlebars.compile(source);
-var context={
-  list:[
-    
-    // {buttonText:'Add to Cart', galleryItemPrice:'$195.00'},
-    // {buttonText:'Add to Cart', galleryItemPrice:'$195.00'},
-    // {buttonText:'Add to Cart', galleryItemPrice:'$195.00'},
-    // {buttonText:'Add to Cart', galleryItemPrice:'$195.00'},
-    // {buttonText:'Add to Cart', galleryItemPrice:'$195.00'},
-    // {buttonText:'Add to Cart', galleryItemPrice:'$195.00'},
-    // {buttonText:'Add to Cart', galleryItemPrice:'$195.00'},
-    // {buttonText:'Add to Cart', galleryItemPrice:'$195.00'},
-    
-  ]
+var context = {
+  list: [],
 };
 
-fetch('https://api.punkapi.com/v2/beers')
-.then(data=>{
-  return data.json()
-})
-.then(data=>{
-  beers = [...data];
+//default api call
 
-  beers.forEach(a=>{
-    console.log(a);
-    if(a.image_url!=='https://images.punkapi.com/v2/keg.png'){
-
-      context.list.push({buttonText:'Add to Cart', galleryItemPrice:`${a.attenuation_level}$`, imgLink:a.image_url});
-    }
-
-   
-
-
-  var html = template(context);
-  document.querySelector('.galleryGallery').innerHTML = html;
-
-
-
-
+fetch("https://api.punkapi.com/v2/beers?per_page=10")
+  .then((data) => {
+    return data.json();
   })
-})
-.catch(err=>{
-  console.log(err);
+  .then((data) => {
+    beers = [...data];
+
+    beers.forEach((a) => {
+      context.list.push({
+        buttonText: "Add to Cart",
+        galleryItemPrice: `${a.attenuation_level}$`,
+        imgLink: a.image_url,
+      });
+      var html = template(context);
+      document.querySelector(".galleryGallery").innerHTML = html;
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//filter api
+document.querySelector(".sorting").addEventListener("change", (e) => {
+  let selectValue = document.querySelector(".sorting").value;
+  context.list = [];
+  if (selectValue == "Show 10") {
+    fetch("https://api.punkapi.com/v2/beers?per_page=10")
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        beers = [...data];
+
+        beers.forEach((a) => {
+          context.list.push({
+            buttonText: "Add to Cart",
+            galleryItemPrice: `${a.attenuation_level}$`,
+            imgLink: a.image_url,
+          });
+          var html = template(context);
+          document.querySelector(".galleryGallery").innerHTML = html;
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  if (selectValue == "Show 20") {
+    context.list = [];
+    console.log("opet fetch");
+    fetch("https://api.punkapi.com/v2/beers?per_page=20")
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        beers = [...data];
+
+        beers.forEach((a) => {
+          context.list.push({
+            buttonText: "Add to Cart",
+            galleryItemPrice: `${a.attenuation_level}$`,
+            imgLink: a.image_url,
+          });
+          var html = template(context);
+
+          document.querySelector(".galleryGallery").innerHTML = html;
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  if (selectValue == "Show all") {
+    context.list = [];
+    fetch("https://api.punkapi.com/v2/beers?per_page=80")
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        beers = [...data];
+
+        beers.forEach((a) => {
+          context.list.push({
+            buttonText: "Add to Cart",
+            galleryItemPrice: `${a.attenuation_level}$`,
+            imgLink: a.image_url,
+          });
+          var html = template(context);
+          document.querySelector(".galleryGallery").innerHTML = html;
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 });
 
-
-
-
-
-
-
- 
+let sorting = () => {};
