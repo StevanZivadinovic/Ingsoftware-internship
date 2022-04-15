f475e0737bc7610d8feab37f6f1154a610e65e91
StevanZivadinovic <stevanzivadinovic@outlook.com>
2022-04-15 15:29:22 +0200

    add beers to cart

--- Birra/script/addToCart.js
+++ Birra/script/addToCart.js
@@ -1,6 +1,6 @@
 let getNumberOfBeers = (e) => {
   inputValue = 0;
-  console.log("evo me");
+  console.log('pozdrav');
   let cartValue = parseInt(document.querySelector(".spanCart").innerHTML);
   var inputValue = parseInt(
     e.target.parentElement.parentElement.children[1].children[2].value
@@ -13,27 +13,45 @@ let getNumberOfBeers = (e) => {
 
 
 
-let getBeersDetails = ()=>{
+let getBeersDetails = (e)=>{
+ let beer =  e.parentElement.parentElement.parentElement.parentElement.previousElementSibling;
+  let imgURL = beer.children[0].children[0].style.backgroundImage.replace(/(url\(|\)|")/g, '');
+ let name = beer.children[1].children[0].innerHTML;
+ let quantity = e.parentElement.parentElement.children[1].children[2].value;
+ console.log(quantity);
 
-  console.log('moj moj')
+
   document.querySelector('.cartListMain').innerHTML+=`<li>
   <div class="listCart">
     <div class="nameList">
-      <h4>Name</h4>
-      <p>Quantity: <span class="spanList">2</span> </p>
+      <h4>${name}</h4>
+      <p>Quantity: <span class="spanList">${quantity}</span> </p>
     </div>
     <div class="contentList">
-      <img src="" alt="" srcset="" style="width: 3rem;">
-      <button>X</button>
+      <img src="${imgURL}" alt="" srcset="" style="width: 2rem;">
+      <button class='listBtnRemove'>X</button>
     </div>
   </div>
-  </li>`
+  </li>`;
+
+  
 }
+let removeBeer = (e)=>{
+
+  
+    if(e.target.classList.contains('listBtnRemove')){
+      console.log(e.target.parentElement);
+      e.target.parentElement.parentElement.remove();
+    }
+   
+}
+document.querySelector('.cartListMain').addEventListener('click', removeBeer)
 
 export let addToCart = () => {
   setTimeout(() => {
     document.querySelectorAll(".smallModalBtn").forEach((a) => {
-      a.addEventListener("click", (getNumberOfBeers, getBeersDetails()), false);
+      a.addEventListener("click", ()=>{getBeersDetails(a)});
+      a.addEventListener("click", getNumberOfBeers);
     });
   }, 100);
 };
