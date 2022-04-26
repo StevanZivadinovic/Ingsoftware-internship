c6680a43682daa3526ca90d78f5d423c1fbb7570
StevanZivadinovic <stevanzivadinovic@outlook.com>
2022-04-26 16:00:12 +0200

    update cart

--- Birra/script/linkToIndividualShop.js
+++ Birra/script/linkToIndividualShop.js
@@ -8,7 +8,7 @@ export let linkTagToIndividualShop = ()=>{
              let idBeer = parseInt(e.target.getAttribute('data-id').slice(3));
              localStorage.setItem('idBeer', idBeer);
     
-             window.location.href = 'http://127.0.0.1:5500/indexIndividualShop.html'
+             window.location.href = 'indexIndividualShop.html'
             })
         })
         
