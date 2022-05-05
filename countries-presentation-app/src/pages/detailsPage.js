db49679ba41c590e9e7570ad4de9e4ff20d847e6
StevanZivadinovic <stevanzivadinovic@outlook.com>
2022-05-05 16:49:22 +0200

    update details page

--- countries-presentation-app/src/pages/detailsPage.js
+++ countries-presentation-app/src/pages/detailsPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { Routes, Route, useParams } from "react-router-dom";
+import {useParams, useNavigate } from "react-router-dom";
 import {specificCountry, getBorderCountries} from './../components/getData';
 import './../style/components/_detailsPage.scss'
 
@@ -12,6 +12,8 @@ const DetailsPage = () => {
     const [borderCountries, setBorderCountries] = useState([]);
     const [fullNameBorderCountries, setFullNameBorderCountries] = useState([])
     const [countries, setCountries] = useState(0)
+
+    const navigate = useNavigate()
 
     useEffect(() => {
       specificCountry(param.id).then(res=>{
@@ -36,17 +38,16 @@ const DetailsPage = () => {
         })
     });
   }, [borderCountries]);
-              
+  console.log(fullNameBorderCountries);
             
 
   return (
     <div className='mainDetailsPage'>
       <div className="backButtonMenu">
-        <button className="backButton"> <span><i className="fa-solid fa-arrow-left"></i> </span> Back</button>
+        <button onClick={()=>{navigate('/')}} className="backButton"> <span><i className="fa-solid fa-arrow-left"></i> </span> Back</button>
       </div>
      {country[0] && <div className="mainContent">
         <div className="bigFlag" style={{backgroundImage:`url(${country[0]?.flag}`}}>
-          {/* <img src={country[0]?.flag} alt='big flag'  /> */}
         </div>
         <div className="detailsOfCountry">
           <h2 className="nameCountry">{param.id}</h2>
@@ -69,10 +70,10 @@ const DetailsPage = () => {
             </div>
           </div>
           <div className="borderCountry">
-            Border Countries: {fullNameBorderCountries && fullNameBorderCountries.map((a,i)=>{
+            Border Countries: {fullNameBorderCountries.length>0 ? fullNameBorderCountries.map((a,i)=>{
               // console.log(a)
             return i<= fullNameBorderCountries.length/2-1 && <span key={i}> {a}</span>
-            })}
+            }) : <span>This country has no neighbors</span>}
           </div>
         </div>
       </div>
